d62b65cdb16b0fa61b470ee180f2d20fb2c368de
ehassah <ehassah@gmail.com>
2019-04-22 23:52:49 -0700

    first commit

---
+++ Service/SearchingHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace SearchingHost
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (ServiceHost host = new ServiceHost(typeof(SearchingService.SearchService)))
+            {
+                host.Open();
+                Console.WriteLine("Host startes @" + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
